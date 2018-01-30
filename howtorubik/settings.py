f5b3f9cc1089c33cacd6e7c66b9c155a5b10ab1c
sashikanth <sashirubik@gmail.com>
2018-01-30 13:39:15 +0530

    Made migrations to heroku

--- howtorubik/settings.py
+++ howtorubik/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'howtorubik.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rubikPost',                      
+        'USER': 'postgres',
+        'PASSWORD': 'password000',
+        'HOST': 'howtorubik.herokuapp.com',
+        'PORT': '8001',
     }
 }
 
