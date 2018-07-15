59e85662cee068cd71ec8d8d9ca088c846a58692
satyasashi <sashikanthpeddinti@gmail.com>
2018-07-15 16:25:06 +0530

    Set Debug to True because it's not working without it

--- howtorubik/settings.py
+++ howtorubik/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'Your_security_key_goes_here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'howtorubik.herokuapp.com']
 
